73e9c26f9e2f90d765f135b97a516d8dc42c7304
jewelrana <tonujewel@gmail.com>
2022-10-16 22:43:13 +0600

    [added]: longest string

--- src/LongestString.java
+++ src/LongestString.java
@@ -1,3 +1,13 @@
 public class LongestString {
-    
+    public static void main(String[] args) {
+        String[] myString = {"I", "love", "my country", "very", "much"};
+
+       for (int i = 0; i < myString.length-1; i++) {
+
+        if (myString[i].length()>myString[i+1].length()) {
+            System.out.println(myString[i]);
+        }
+         
+       }
+    }
 }
