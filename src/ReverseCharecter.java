38be2fd1f0262cc8900dc4393a355e072b1eb7ff
jewelrana <tonujewel@gmail.com>
2022-10-18 23:34:16 +0600

    [added]: reverse char

---
+++ src/ReverseCharecter.java
@@ -0,0 +1,17 @@
+public class ReverseCharecter {
+    public static void main(String[] args) {
+        String sentence = "I love programming";
+        reverChar(sentence);
+    }
+
+  static  void reverChar(String character){
+
+    String[] newData = character.split(" ");
+
+    for (int i = 0; i < newData.length; i++) {
+
+    }
+    
+
+    }
+}
