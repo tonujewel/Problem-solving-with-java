eab734d4f392e170975b87c47df25c32cc8c0583
jewelrana <tonujewel@gmail.com>
2022-10-22 02:03:15 +0600

    [fix] : ContainDuplicate time complexity

--- src/ContainDuplicate.java
+++ src/ContainDuplicate.java
@@ -1,7 +1,10 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainDuplicate {
 
     public static void main(String[] args) {
-        int[] data = { 1,2,3,1};
+        int[] data = { 1, 2, 3, 1 };
         containsDuplicate(data);
     }
 
@@ -9,26 +12,25 @@ public class ContainDuplicate {
         boolean value = false;
         for (int i = 0; i < nums.length-1; i++) {
 
-            System.out.print(nums[i]);
-            System.out.print(nums[i+1]);
-            System.out.println(" ");
-
-            while (value) {
-                
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    value = true;
+                    break;
+                } 
             }
-
-            if (nums[i]==nums[i+1]) {
-                value=true;
-            }
-
-            // for (int j = i + 1; j < nums.length; j++) {
-            //     if (nums[i] == nums[j]) {
-            //         value = true;
-            //     } 
-            // }
         }
-    System.out.println(value);
+    
         return value;
+
+    }
+
+    public boolean containsDuplicate2(int[] nums) {
+        Set<Integer> seen = new HashSet();
+        for (int n : nums) {
+            if (!seen.add(n))
+                return true;
+        }
+        return false;
     }
 
 }
